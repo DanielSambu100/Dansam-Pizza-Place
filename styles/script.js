9930d6fc55890eb8ca7580c9799da1d3965adb6d
Danielsambu100 <daniel.wanyonyi@student.moringaschool.com>
2022-03-22 10:24:28 +0300

    update

---
+++ styles/script.js
@@ -0,0 +1,18 @@
+$(function () {
+    $('.summary').hide();
+    $('.customer-data').hide();
+//Get inputs
+    $("#checkout").click(function () {
+        let flavour = $(".flavour option:selected").val();
+        let size = $("#size option:selected").val();
+        let crust = $("#crust option:selected").val();
+        let topping = $("#toppings option:selected").val();
+        let number = $("#number").val();
+        console.log(size);
+
+        //Function order
+        let order = (f, s, c, t, n, total) => {
+            return {f, s, c, t, n, total};
+        };
+
+     
